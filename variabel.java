d24af5a5560d6777ee38d9cd877a29527789961b
Mahayoga <144007456+Mahayoga@users.noreply.github.com>
2023-09-05 12:16:39 +0700

    Add files via upload

---
+++ variabel.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Variabel;
+
+/**
+ *
+ * @author Personal
+ */
+public class variabel {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int umur = 24;
+        int UMUR = 25;
+        System.out.println(umur); // prints 24
+        System.out.println(UMUR); // prints 25
+    }
+}
